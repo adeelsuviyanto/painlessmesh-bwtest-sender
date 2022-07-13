6508c021208cdc30c1bfd3069a29b6e9fc2c9318
adeelsuviyanto <adeelsuviyanto@gmail.com>
2022-07-13 23:42:10 +0700

    Second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,46 @@
+/*
+ * ESP8266-based PainlessMesh Bandwidth Test Program
+ * Muhammad Adeel Mahdi Suviyanto
+ * Telkom University
+ * 
+ * Sender Node
+ */
+
 #include <Arduino.h>
+#include <painlessMesh.h>
+
+#define MESH_PREFIX "BW_TEST_PAINLESSMESH"
+#define MESH_PASS "12345678"
+#define MESH_PORT 5555
+
+int arrayToSend[256];
+painlessMesh mesh;
+Scheduler userScheduler;
+
+void newConnectionCallback(uint32_t nodeId){
+  Serial.printf("New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void receivedCallback(uint32_t from, String &msg){
+  Serial.printf("Message from %u msg=%s\n", from, msg.c_str());
+}
+
+void bandwidthTest(){
+  
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );
+  mesh.init(MESH_PREFIX, MESH_PASS, MESH_PORT);
 }
 
 void loop() {
